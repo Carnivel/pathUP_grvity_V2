aa92858dccd9441bda21c50d8dec0edbe4ce63fb
anandhusg <ananthusg12@gmail.com>
2026-06-17 18:09:10 +0530

    fixed bugs found on header audit

--- next_frontend/next.config.mjs
+++ next_frontend/next.config.mjs
@@ -26,6 +26,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
